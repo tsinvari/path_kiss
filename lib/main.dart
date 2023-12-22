7faf3179d3b17146fc18fa50cc5a937a9f91b84c
tsinvari <solre.mifa@gmail.com>
2023-12-22 17:44:15 -0300

    first draft

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The patth to a Kiss',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'The path to a Kiss'),
     );
   }
 }
@@ -39,87 +24,193 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+  Map cardText = {
+      'title': ['Elije un poema'],
+      'name': ['Deseo','Ojos','Manos','Beso'],
+      'first': ['Deseo1','Ojos1','Manos1','Beso1'],
+      'url': ['url_deseo_sp','url_ojos_sp','url_manos_sp','url_beso_pt'],
+      'buttons': ['Leer & escuchar', 'Sólo escuchar']
+  };
+  final List<Color> cardColors = [
+    Colors.amber,
+    Colors.blueGrey,
+    Colors.orange,
+    Colors.lightBlue,
+  ];
+
+  final List<String> languages = [
+    "Español","English","Portugues", "Français"
+  ];
+
+    void nextCard() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      currentIndex = (currentIndex + 1) % 4;
     });
   }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              cardText['title'][0],
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20.0),
+            alignment: Alignment.center,
+            height: 300,
+            child: GestureDetector(
+              onTap: nextCard,
+              child: Stack(
+                children: <Widget>[
+                  for (int i = 0; i < 4; i++)
+                    if (currentIndex == i)
+                      Positioned(
+                        top: 20.0 * i,
+                        left: 20.0 * i,
+                        child: Container(
+                          width: 300,
+                          child: Card(
+                            elevation: 4.0,
+                            color: cardColors[i],
+                            child: Column (
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                 ListTile(
+                                  leading: Icon(Icons.soap),
+                                  title: Text(cardText['name'][i]),
+                                  subtitle: Text(cardText['first'][i]),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    TextButton(
+                                      onPressed: (){
+                                        showDialog(
+                                          context: context, 
+                                          builder: (BuildContext context){
+                                            return AlertDialog(
+                                              title: Text(cardText['name'][i]),
+                                              content: Text('Lorem Ipso'),
+                                              actions: <Widget>[
+                                                TextButton(
+                                                  onPressed: (){Navigator.of(context).pop();}, 
+                                                  child: Text('Close'))
+                                              ],
+                                            );
+                                          }
+                                          );
+                                    }, 
+                                    child: Text(cardText['buttons'][0])),
+                                    TextButton(
+                                      onPressed: (){
+                                        var snackBar = SnackBar(content: Text(cardText['url'][i]));
+                                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                      }, child: Text(cardText['buttons'][1])),
+                                  ],
+                                )
+                              ],
+                            )
+                            ),
+                        )
+                      ),
+                ],
+              ),
             ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20.0),
+            child: DropdownButton<String>(
+            hint: Text("Language"),
+            value: null,
+            items: languages.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            onChanged: (String? value) {
+                switch (value){
+                  case null:
+                  setState(() {
+                      cardText['title']= ['Elije un poema'];
+                      cardText['name'] = ['Deseo','Ojos','Manos','Beso'];
+                      cardText ['first']= ['Deseo1','Ojos1','Manos1','Beso1'];
+                      cardText['url']= ['url_deseo_sp','url_ojos_sp','url_manos_sp','url_beso_pt'];
+                      cardText['buttons']= ['Leer & escuchar', 'Sólo Escuchar'];
+                    });
+                  break;
+                  case "Español":
+                  setState(() {
+                      cardText['title']= ['Elije un poema'];
+                      cardText['name'] = ['Deseo','Ojos','Manos','Beso'];
+                      cardText ['first']= ['Deseo1','Ojos1','Manos1','Beso1'];
+                      cardText['url']= ['url_deseo_sp','url_ojos_sp','url_manos_sp','url_beso_pt'];
+                      cardText['buttons']= ['Leer & escuchar', 'Sólo Escuchar'];
+                    });
+                  break;
+                  case "English":
+                  setState(() {
+                      cardText['title'] = ['Choose a poem'];
+                      cardText['name']= ['Desire', 'Eyes', 'Hands', 'Kiss'];
+                      cardText['first']= ['Desire1', 'Eyes1', 'Hands1', 'Kiss1'];
+                      cardText['url']= ['url_deseo_en','url_ojos_en','url_manos_en','url_beso_en'];
+                      cardText['buttons'] = ['Read & Listen', 'Listen Only'];
+
+                   });
+                  break;
+                  case "Portugues":
+                  setState(() {
+                      cardText['title'] = ['Escolha um poema'];
+                      cardText['name']= ['Desejo', 'Olhos', 'Mãos', 'Beijo'];
+                      cardText['first']= ['Desejo1', 'Olhos1', 'Mãos1', 'Beijo1'];
+                      cardText['url']= ['url_deseo_pt','url_ojos_pt','url_manos_pt','url_beso_pt'];
+                      cardText['buttons'] = ['Ler & Ouvir', 'Apenas Ouvir'];
+                          });
+                  break;
+                  case "Français":
+                  setState(() {
+                      cardText['title'] = ['Choisissez un poème'];
+                      cardText['name']= ['Désir', 'Yeux', 'Mains', 'Baiser'];
+                      cardText['first']= ['Désir1', 'Yeux1', 'Mains1', 'Baiser1'];
+                      cardText['url']= ['url_deseo_sp','url_ojos_sp','url_manos_','url_beso_fr'];
+                      cardText['buttons'] = ['Lire & Écouter', 'Écouter Seulement'];
+                  });
+                  break;
+                }                
+              }),
+          )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+     /* floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),  This trailing comma makes auto-formatting nicer for build methods.*/
     );
   }
 }
